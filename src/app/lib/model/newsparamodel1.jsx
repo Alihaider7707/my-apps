25e8afa72e08815146505b8d5ac8cfe1f7eec50b
Ali Haider <108930936+FHGROUPOC@users.noreply.github.com>
2023-12-23 14:00:38 +0500

    alihaider

--- src/app/lib/model/newsparamodel1.jsx
+++ src/app/lib/model/newsparamodel1.jsx
@@ -8,7 +8,7 @@ const mymodel = new mongoose.Schema({
     Newsheading1: String,
     Newsdecription1: String
 
-}, { timestamps: { createdAt: true, updatedAt: false }  });
+}, { timestamps:  true  });
 
 
 export const NewsparaModel1 = mongoose.models.newscollection1s || mongoose.model("newscollection1s", mymodel)
